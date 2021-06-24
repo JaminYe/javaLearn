2db73a83ca5bedff15d461d971b91923b9ea4ef0
JaminYe <JaminYe2018@outlook.com>
2021-06-24 18:54:05 +0800

    xxl-job

--- xxl-job-demo/src/main/java/cn/jaminye/xxljobdemo/XxlJobDemoApplication.java
+++ xxl-job-demo/src/main/java/cn/jaminye/xxljobdemo/XxlJobDemoApplication.java
@@ -3,11 +3,13 @@ package cn.jaminye.xxljobdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.CountDownLatch;
+
 @SpringBootApplication
 public class XxlJobDemoApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(XxlJobDemoApplication.class, args);
+		//让程序不结束
+		new CountDownLatch(1).await();
 	}
-
 }
--- xxl-job-demo/src/main/java/cn/jaminye/xxljobdemo/config/XxlJobConfig.java
+++ xxl-job-demo/src/main/java/cn/jaminye/xxljobdemo/config/XxlJobConfig.java
@@ -2,6 +2,7 @@ package cn.jaminye.xxljobdemo.config;
 
 import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,15 +12,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class XxlJobConfig {
 
-	@Value("${xxl.job.executor.adminAdresses}")
+	@Value("${xxl.job.admin.addresses}")
 	private String adminAdresses;
 
 	@Value("${xxl.job.executor.appname}")
 	private String appname;
 
+	@Bean
 	public XxlJobSpringExecutor xxlJobSpringExecutor() {
 		XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
 		xxlJobSpringExecutor.setAdminAddresses(adminAdresses);
+		xxlJobSpringExecutor.setAppname(appname);
 		return xxlJobSpringExecutor;
 	}
 
