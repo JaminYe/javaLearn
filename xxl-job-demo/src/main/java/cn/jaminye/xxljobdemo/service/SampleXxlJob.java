fd18380c282d0cbeee1efcb135a3441b2b240d5b
JaminYe <JaminYe2018@outlook.com>
2021-06-29 17:05:53 +0800

    xxl-job结束

--- xxl-job-demo/src/main/java/cn/jaminye/xxljobdemo/service/SampleXxlJob.java
+++ xxl-job-demo/src/main/java/cn/jaminye/xxljobdemo/service/SampleXxlJob.java
@@ -1,6 +1,9 @@
 package cn.jaminye.xxljobdemo.service;
 
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,10 +12,15 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SampleXxlJob {
+	static Logger logger = LoggerFactory.getLogger(SampleXxlJob.class);
 
 	@XxlJob("demoJobHandler")
-	public void demoJobHandler() {
-		System.out.println("任务执行成功");
+	public void demoJobHandler() throws InterruptedException {
+		//控制台日志
+		logger.info("123控制台打印");
+		// 执行日志
+		XxlJobHelper.log("123执行日志打印");
+		Thread.sleep(2000);
+		XxlJobHelper.handleFail("123");
 	}
-
 }
