4ea4d17a0a8f16aae7ff985f85a9cfd975e2ef3b
JaminYe <JaminYe2018@outlook.com>
2021-07-13 21:57:12 +0800

    confirm使用

--- rabbitmq/rabbitmq-base/src/main/java/cn/jaminye/rabbitmqbase/confirm/ConfirmConsumerOne.java
+++ rabbitmq/rabbitmq-base/src/main/java/cn/jaminye/rabbitmqbase/confirm/ConfirmConsumerOne.java
@@ -1,4 +1,4 @@
-package cn.jaminye.rabbitmqbase.topic;
+package cn.jaminye.rabbitmqbase.confirm;
 
 import cn.jaminye.rabbitmqbase.util.RabbitMqUtil;
 import com.rabbitmq.client.*;
@@ -10,17 +10,15 @@ import java.util.concurrent.TimeoutException;
  * @author jaminye
  * @date 2021/7/12 下午2:56
  */
-public class TopicConsumerOne {
+public class ConfirmConsumerOne {
 	public static void main(String[] args) throws IOException, TimeoutException {
 		Connection connection = RabbitMqUtil.getConnection();
 		Channel channel = connection.createChannel();
 		//创建队列
-		// channel.queueDeclare("topicTwo",false,false,false,null);
-		channel.queueDeclare("topicOne", false, false, false, null);
+		channel.queueDeclare("confirmOne", false, false, false, null);
 		//topic交换机 绑定交换机路由key
-		// channel.queueBind("topicTwo","topic交换机","#.weather");
-		channel.queueBind("topicOne", "topic交换机", "china.anhui.hefei.*");
-		channel.basicConsume("topicOne", new DefaultConsumer(channel) {
+		channel.queueBind("confirmOne", "confirm交换机", "china.anhui.hefei.*");
+		channel.basicConsume("confirmOne", new DefaultConsumer(channel) {
 			@Override
 			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
 				System.out.println("========消息========>" + new String(body));
---
+++ rabbitmq/rabbitmq-base/src/main/java/cn/jaminye/rabbitmqbase/confirm/ConfirmProduct.java
@@ -0,0 +1,58 @@
+package cn.jaminye.rabbitmqbase.confirm;
+
+import cn.jaminye.rabbitmqbase.util.RabbitMqUtil;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author Jamin
+ * @date 2021/7/13 15:00
+ */
+public class ConfirmProduct {
+	public static void main(String[] args) throws IOException, TimeoutException {
+		Connection connection = RabbitMqUtil.getConnection();
+		Channel channel = connection.createChannel();
+		//创建一个交换机topic模式
+		// channel.exchangeDeclare("topic交换机", BuiltinExchangeType.TOPIC);
+		HashMap<String, String> map = new HashMap<String, String>(12);
+		map.put("china.anhui.hefei.weather", "合肥的天气情况");
+		map.put("china.anhui.wuhu.weather", "芜湖的天气情况");
+		map.put("china.jiangsu.nanjing.weather", "南京的天气情况");
+		map.put("china.jiangsu.suzhou.weather", "苏州的天气情况");
+		map.put("china.anhui.hefei.news", "合肥的新闻情况");
+		map.put("china.jiangsu.nanjing.news", "南京的新闻情况");
+		channel.confirmSelect();
+		channel.addConfirmListener(new ConfirmListener() {
+			@Override
+			public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+				System.out.println("发送成功的消息ID========>" + deliveryTag);
+			}
+
+			@Override
+			public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+				System.out.println("发送失败的消息ID========>" + deliveryTag);
+
+			}
+		});
+		channel.addReturnListener((replyCode, replyText, exchange, routingKey, properties, body) -> {
+			System.err.println("===========未可达===========>" + new String(body));
+		});
+		map.entrySet().forEach(map1 -> {
+			try {
+				//发布到交换机    指定routingkey  mandatory指定为true
+				channel.basicPublish("confirm交换机", map1.getKey(), true, null, map1.getValue().getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+		//
+		// channel.close();
+		// connection.close();
+		System.out.println("消息发送成功");
+	}
+}
