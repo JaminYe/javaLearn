50c4123221d928c373abcdbfb8a627dca8dff746
jaminye <JaminYe2018@outlook.com>
2022-02-15 21:55:30 +0800

    未知提交

--- kafka/kafka-demo/src/main/java/cn/jaminye/kafkademo/kafka/MsgProducerTest.java
+++ kafka/kafka-demo/src/main/java/cn/jaminye/kafkademo/kafka/MsgProducerTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -14,64 +15,66 @@ import java.util.concurrent.ExecutionException;
  * 生产者
  */
 public class MsgProducerTest {
-    KafkaProducer<String, String> producer;
+	KafkaProducer<String, String> producer;
 
-    @Before
-    public void init() {
-        Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.211.55.4:9092");
-        //0 不需要回复,1等待leader写入log 不需要follower -1 等待集群全部写入log
-        properties.put(ProducerConfig.ACKS_CONFIG, "1");
-        //失败重试时间
-        properties.put(ProducerConfig.RETRIES_CONFIG, "3");
-        //本地缓存区时间
-        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
-        //批量发送大小,批量满16k就发送
-        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
-        //100毫秒批量未填充完成也发送
-        properties.put(ProducerConfig.LINGER_MS_CONFIG, "100");
-        //发送key从字符串转字节数组
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        producer = new KafkaProducer<>(properties);
-    }
+	@Before
+	public void init() {
+		Properties properties = new Properties();
+		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.211.55.22:9092,10.211.55.22:9093,10.211.55.22:9094");
+		/**
+		 * 0 不需要回复
+		 * 1等待leader写入log不需要follower
+		 * -1 等待集群全部写入log
+		 */
+		properties.put(ProducerConfig.ACKS_CONFIG, "1");
+		// 失败重试时间
+		properties.put(ProducerConfig.RETRIES_CONFIG, "3");
+		// 本地缓存区时间
+		properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
+		// 批量发送大小,批量满16k就发送
+		properties.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+		//100毫秒批量未填充完成也发送
+		properties.put(ProducerConfig.LINGER_MS_CONFIG, "10");
+		//发送key从字符串转字节数组
+		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		producer = new KafkaProducer<>(properties);
+	}
 
-    @Test
-    public void sendMsg() {
-        for (int i = 0; i < 10; i++) {
-            producer.send(new ProducerRecord<String, String>("test-topic", "test-key" + i, "key-value" + i));
-        }
-        producer.close();
-    }
-
-
-    @Test
-    public void sendMsg1() throws ExecutionException, InterruptedException {
-        for (int i = 0; i < 100; i++) {
-            ProducerRecord<String, String> stringStringProducerRecord = new ProducerRecord<>("order-topic", 0, "test" + i, "test" + i);
-            RecordMetadata recordMetadata = producer.send(stringStringProducerRecord).get();
-            System.out.println("发送成功：" + "topic：" + recordMetadata.topic() + "partition:" + recordMetadata.partition() + "offset:" + recordMetadata.offset());
-        }
-
-    }
+	@Test
+	public void sendMsg() {
+		for (int i = 0; i < 10; i++) {
+			producer.send(new ProducerRecord<String, String>("test-topic", "test-key" + i, "key-value" + i));
+		}
+		producer.close();
+	}
 
 
+	@Test
+	public void sendMsg1() throws ExecutionException, InterruptedException {
+		for (int i = 0; i < 100; i++) {
+			ProducerRecord<String, String> stringStringProducerRecord = new ProducerRecord<>("order-topic", 0, "test" + i, "test" + i);
+			RecordMetadata recordMetadata = producer.send(stringStringProducerRecord).get();
+			System.out.println("发送成功：" + "topic：" + recordMetadata.topic() + "partition:" + recordMetadata.partition() + "offset:" + recordMetadata.offset());
+		}
+
+	}
 
 
-    @Test
-	public  void sendMsg2() throws InterruptedException {
+	@Test
+	public void sendMsg2() throws InterruptedException {
+		final CountDownLatch countDownLatch = new CountDownLatch(1);
 		ProducerRecord<String, String> stringStringProducerRecord = new ProducerRecord<>("yibu-topic", 0, "test", "test");
-		producer.send(stringStringProducerRecord, new Callback() {
-			@Override
-			public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-			if (e!=null){
+		producer.send(stringStringProducerRecord, (recordMetadata, e) -> {
+			if (e != null) {
 				System.err.println("发送信息失败");
 			}
-			if (recordMetadata!=null){
-					System.out.println("topic："+recordMetadata.topic()+"partition"+recordMetadata.partition()+"offset:"+recordMetadata.offset());
-				}
+			if (recordMetadata != null) {
+				System.out.println("topic：" + recordMetadata.topic() + "partition" + recordMetadata.partition() + "offset:" + recordMetadata.offset());
 			}
+			countDownLatch.countDown();
 		});
-		Thread.sleep(Integer.MAX_VALUE);
+		countDownLatch.await();
+		producer.close();
 	}
 }
