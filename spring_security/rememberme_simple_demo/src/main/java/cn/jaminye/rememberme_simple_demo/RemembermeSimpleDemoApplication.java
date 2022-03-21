005467f849c84fcd690888b9a5f772ac917f2a87
JaminYe <JaminYe2018@outlook.com>
2022-03-21 15:44:14 +0800

    Spring Security

---
+++ spring_security/rememberme_simple_demo/src/main/java/cn/jaminye/rememberme_simple_demo/RemembermeSimpleDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.jaminye.rememberme_simple_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemembermeSimpleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemembermeSimpleDemoApplication.class, args);
+	}
+
+}
